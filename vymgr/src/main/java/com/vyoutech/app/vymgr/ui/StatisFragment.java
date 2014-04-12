45c8f9305c95c0b32b195692f10f08ab9bd13512
loulis <loulisy@gmail.com>
2014-04-12 17:16:38 +0800

    增加viewpager的 fragment

--- vymgr/src/main/java/com/vyoutech/app/vymgr/ui/StatisFragment.java
+++ vymgr/src/main/java/com/vyoutech/app/vymgr/ui/StatisFragment.java
@@ -1,10 +1,28 @@
 package com.vyoutech.app.vymgr.ui;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vyoutech.app.vymgr.R;
+
 
 /**
  * Created by loulis on 2014/4/12.
  */
-public class StatisFragment extends Fragment
+public class StatisFragment extends ListFragment
 {
+    private View mEmptyView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_list_with_empty_container, container, false);
+        mEmptyView = rootView.findViewById(android.R.id.empty);
+        inflater.inflate(R.layout.empty_waiting_for_sync, (ViewGroup) mEmptyView, true);
+        return rootView;
+    }
 }
